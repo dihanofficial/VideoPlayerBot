e06ff1dc83f1e70467c32509a6168819a941f38f
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-27 06:32:43 +0530

    Delete plugins directory

--- plugins/tr.py
+++
@@ -1,8 +0,0 @@
-import os
-from config import ASSISTANT_NAME
-
-HELP_TEXT =f"**SETTINGS** \n\n`1) Add this Bot to your Group and Make it Admin` \n2) **Add** @{ASSISTANT_NAME} **to your Group** \n\n**COMMANDS** \n\n- `/vplay` **: Reply to Video or File That You Want To stream In Vc.** \n- `/stop`  **: Stop the stream**"
-
-START_TEXT = '👋 `Hey My Name Is` **Video Player Bot** \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
-
-ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 This bot was created to stream video in telegram group video chats using several methods from WebRTC.` \n\n`👨‍🔧 Powered by PyTgcalls the Async client API for the Telegram Group Calls, and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots.` \n\n**This bot licensed under GNU-GPL 3.0 License**'
